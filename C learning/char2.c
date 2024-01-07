dfcc75550f0d7f574d11c90dec94e5b3259f6af5
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-01-07 15:10:29 +0530

    Update char2.c: Added ASCII code calculations and output

--- C learning/char2.c
+++ C learning/char2.c
@@ -2,8 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-    char ch= 'a';
-    printf("ASCII code of %c is %d\n", ch, ch);
+    char ch= 'a', ch1 = 'A';
+    printf("ASCII code of %c & %c is %d & %d\n", ch, ch1, ch, ch1);
+    
+    printf("%c + %c = %c\n", ch, ch1, ch+ch1);
+    printf("ASCII code of %c is %d\n", ch+ch1, ch+ch1);
     
     return 0;
 }
