69d70e5b4a99b55fd90f730c8a700a5354f883de
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-01-10 00:22:39 +0530

    Add linear search implementation

---
+++ PPS LAB/24.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int len, int key) {
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i; // Element found at index i
+        }
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int key;
+    
+    printf("Enter element to search: ");    
+    scanf("%d", &key);
+
+    int result = linearSearch(arr, len, key);
+
+    if (result == -1)
+    {
+        printf("Element not found\n");
+    }
+
+    else
+    {
+        printf("Element found at index %d\n", result);
+    }
+
+    return 0;
+}
