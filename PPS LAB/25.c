1284e2888ffecfb2aa06a7885d59539761075f43
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-01-10 00:36:32 +0530

    Add Bubble Sort program

---
+++ PPS LAB/25.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int len){
+
+    int i, j;
+    for (i = 0; i < len-1; i++) {
+        for (j = 0; j < len-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, len);
+
+    printf("Sorted array: \n");
+
+    for (int i=0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
