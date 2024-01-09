ad1450d944d1ccadbca39b30243aaec3fa32507e
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-01-10 00:13:08 +0530

    Min & max element of array program

---
+++ PPS LAB/23.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Minimum element: %d\n", min);
+    printf("Maximum element: %d\n", max);
+
+    return 0;
+}
