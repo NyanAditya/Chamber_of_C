57eae233e567a3b9c5691789260e20ae265d4561
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-01-24 02:49:51 +0530

    Add student record input and display functionality

--- PPS LAB/Updated_List/32.c
+++ PPS LAB/Updated_List/32.c
@@ -1,3 +1,62 @@
 /*32.	Define a structure data type name STUDENT. The type contains student id: string type, student name: type string, student age:type integer, total marks: float type.
 Display all the record of the student.*/
 
+#include <stdio.h>
+
+struct student
+{
+    char id[15];
+    char name[25];
+    int age;
+    float marks;
+};
+
+int main()
+{
+    int n;
+    printf("Enter no. of students:");
+    scanf("%d", &n);
+
+    struct student s[n];
+
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the details of student %d:\n", i + 1);
+
+        getchar();
+        printf("ID: ");
+        scanf("%s", s[i].id);
+
+        printf("Name: ");
+        gets(s[i].name);
+
+        printf("Age: ");
+        scanf("%d", &s[i].age);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Details of student %d:\n", i + 1);
+
+        printf("ID: %s\n", s[i].id);
+
+        printf("Name: %s\n", s[i].name);
+
+        printf("Age: %d\n", s[i].age);
+
+        printf("Marks: %0.2f\n", s[i].marks);
+
+        printf("\n");
+    }
+
+    return 0;
+}
