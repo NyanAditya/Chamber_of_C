b54e86bd10dde5df1d8ea76323ce81472d6a917a
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-02-03 11:03:00 +0530

    scratch

---
+++ C learning/new.c
@@ -0,0 +1,19 @@
+int main()
+
+{
+
+int k=8; switch(k)
+
+{
+
+case 1==8: printf("“ROSE “"); break;
+
+case 1 && 2: printf("“JASMINE “"); break;
+
+default: printf("“FLOWER “");
+
+}
+
+printf("“GARDEN”");
+
+}
