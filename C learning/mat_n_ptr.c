11c2d9c75b63a49b82899454bc5ef89e94b03499
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-02-18 17:51:25 +0530

    Update modulus_testing.exe

---
+++ C learning/mat_n_ptr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define ROWS 3
+#define COLS 3
+
+void printMatrix(int matrix[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+            printf("(%u) ", &matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[ROWS][COLS] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    printMatrix(matrix);
+
+    return 0;
+}
