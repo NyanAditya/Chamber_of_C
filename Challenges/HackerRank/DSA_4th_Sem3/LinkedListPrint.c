74ffb0e8ffce173e88248b696ffbab812d613389
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-09-30 14:38:20 +0530

    Refactor printLinkedList function to correctly traverse and print the linked list

--- Challenges/HackerRank/DSA_4th_Sem3/LinkedListPrint.c
+++ Challenges/HackerRank/DSA_4th_Sem3/LinkedListPrint.c
@@ -69,7 +69,7 @@ void printLinkedList(SinglyLinkedListNode* head) {
         SinglyLinkedListNode *temp = head;
         while(temp != NULL){
             printf("%d\n", temp->data);
-            ;
+            temp = temp->next;
         }
     }
 
