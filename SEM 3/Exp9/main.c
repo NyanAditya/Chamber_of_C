48b15f5bc3963288de0dace924612d27e344cd2d
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-11-10 19:48:07 +0000

    Fixing radix sort and bucket sort

--- SEM 3/Exp9/main.c
+++ SEM 3/Exp9/main.c
@@ -4,7 +4,7 @@
 void mergeSort(int arr[], int left, int right);
 void radixSort(int arr[], int size);
 void countingSort(int arr[], int size);
-void bucketSort(float arr[], int size);
+void bucketSort(int arr[], int size);
 void heapSort(int arr[], int size);
 
 // Function to display the array (add this in main.c)
@@ -18,7 +18,7 @@ void display(int arr[], int size)
 int main()
 {
     // Array for testing sorting algorithms
-    int arr1[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr1[] = {74, 34, 25, 12, 22, 11, 90, 65, 32, 1};
     int size1 = sizeof(arr1) / sizeof(arr1[0]);
 
     // Merge Sort
@@ -28,24 +28,24 @@ int main()
     printf("Sorted array using Merge Sort: ");
     display(arr1, size1);
 
-    // Reset the array for next sorting
-    int arr2[] = {64, 34, 25, 12, 22, 11, 90};
+    // Radix Sort
+    int arr2[] = {74, 34, 25, 12, 22, 11, 90, 65, 32, 1};
     printf("\nOriginal array for Radix Sort: ");
     display(arr2, size1);
     radixSort(arr2, size1);
     printf("Sorted array using Radix Sort: ");
     display(arr2, size1);
 
-    // Reset the array for next sorting
-    int arr3[] = {64, 34, 25, 12, 22, 11, 90};
+    // Counting Sort
+    int arr3[] = {74, 34, 25, 12, 22, 11, 90, 65, 32, 1};
     printf("\nOriginal array for Counting Sort: ");
     display(arr3, size1);
     countingSort(arr3, size1);
     printf("Sorted array using Counting Sort: ");
     display(arr3, size1);
 
-    // Reset the array for next sorting
-    float arr4[] = {0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23};
+    // Bucket Sort
+    int arr4[] = {74, 34, 25, 12, 22, 11, 90, 65, 32, 1};
     int size4 = sizeof(arr4) / sizeof(arr4[0]);
     printf("\nOriginal array for Bucket Sort: ");
     display(arr4, size4);
@@ -53,8 +53,8 @@ int main()
     printf("Sorted array using Bucket Sort: ");
     display(arr4, size4);
 
-    // Reset the array for next sorting
-    int arr5[] = {64, 34, 25, 12, 22, 11, 90};
+    // Heap Sort
+    int arr5[] = {74, 34, 25, 12, 22, 11, 90, 65, 32, 1};
     int size5 = sizeof(arr5) / sizeof(arr5[0]);
     printf("\nOriginal array for Heap Sort: ");
     display(arr5, size5);
