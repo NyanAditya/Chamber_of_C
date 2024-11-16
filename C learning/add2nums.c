1810ea77394870c7b5dd177f51537c66856c12f7
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-11-16 16:43:47 +0530

    feat: add multiplication functionality and directory creation script; remove unused PDF files

--- C learning/add2nums.c
+++ C learning/add2nums.c
@@ -5,5 +5,11 @@ int main()
     n1 = 10;
     n2 = 20;
     sum = n1 + n2;
+
+    int mul;
+    mul = n1 * n2;
+
+    printf("The sum of %d and %d is %d\n", n1, n2, sum);
+    printf("The multiplication of %d and %d is %d\n", n1, n2, mul);
     return 0;
 }
