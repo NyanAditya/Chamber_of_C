f82246f565d1099b872ef0d0056c800bd3cd4b08
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-11-27 22:49:14 +0530

    feat: add programs to convert infix to postfix and evaluate postfix expressions

---
+++ SEM 3/Exp10/infixTOpostfix.c
@@ -0,0 +1,9 @@
+// Program to convert infix expression to postfix expression    
+
+#include <stdio.h>
+
+#define STACKSIZE 100
+
+int main() { 
+    int main();
+}
---
+++ SEM 3/Exp10/postfix_Eval.c
@@ -0,0 +1,11 @@
+// program to evaluate postfix expression
+
+#include <stdio.h>
+
+#define STACKSIZE 100
+
+int main() {
+    
+
+
+}
