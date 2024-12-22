9a3e54ba75a28b49c406f6aa4f79ba2ef872fceb
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-12-23 01:26:38 +0530

    feat: add a new C program for pattern printing and update VSCode settings for file associations

---
+++ C learning/tests/pat1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int Maxrows = 5;
+
+    // for(int i=1; i<=Maxrows; i++) {
+    //     for(int c=1; c<=i; c++) {
+    //         printf("* ");
+    //     }
+
+    //     printf("\n");
+    // }
+
+    int temp = Maxrows;
+
+    // for(temp; temp>=1; temp--) {
+
+    //     for(int j=1;j<=temp;j++){
+    //         char letter = 'A'-1;
+    //         printf("%c ", letter);
+    //     }
+    //     printf("\n");
+    // }
+
+    printf(printf(1));
+    return 0;
+}
