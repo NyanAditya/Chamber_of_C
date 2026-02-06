de6c095438c94d2c3f213067dccd0e185d499975
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2026-02-06 14:07:12 +0530

    Refactor code structure for improved readability and maintainability

--- C learning/Pointers/ptr_arr.c
+++ C learning/Pointers/ptr_arr.c
@@ -5,4 +5,16 @@ int main() {
     int arr[5] = {10, 20, 30, 40, 50};
 
     int *p_arr = arr;
+
+    printf("Value of arr[0] through p_arr: %d\n", *p_arr);
+    
+    char str[5] = "Hello";
+    char s[] = "Hello";
+
+    printf("Value of str: %s\n", str);
+    printf("Value of s: %s\n", s);
+
+    printf("Value of str[5]: %c\n", str[4]);
+    printf("Value of s[5]: %c\n", s[5]);
+
 }
